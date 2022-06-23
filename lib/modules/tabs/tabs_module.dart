3820e8fc49833568b6f1084df6ce2ee4fc040ff6
Eduardo <eduardocontiero@hotmail.com>
2022-06-23 20:51:07 -0300

    create tabs

--- lib/modules/tabs/tabs_module.dart
+++ lib/modules/tabs/tabs_module.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'discover_page.dart';
+import 'home_page.dart';
+import 'inbox_page.dart';
+import 'profile_page.dart';
 import 'tabs_page.dart';
 
 class TabsModule extends Module {
@@ -9,7 +13,12 @@ class TabsModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-    ChildRoute('/', child: (context, args) => TabsPage())
+    ChildRoute('/', child: (context, args) => TabsPage(), children: [
+      ChildRoute('/home', child: (context, args) => HomePage()),
+      ChildRoute('/discovers', child: (context, args) => DiscoverPage()),
+      ChildRoute('/inbox', child: (context, args) => InboxPage()),
+      ChildRoute('/profile', child: (context, args) => ProfilePage()),
+    ])
   ];
 
 }
--- lib/modules/tabs/tabs_page.dart
+++ lib/modules/tabs/tabs_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class TabsPage extends StatefulWidget {
   const TabsPage({Key? key}) : super(key: key);
@@ -8,8 +9,50 @@ class TabsPage extends StatefulWidget {
 }
 
 class _TabsPageState extends State<TabsPage> {
+  int _pageIndex = 0;
+
+  void _changePage(int page) {
+    setState(() {
+      _pageIndex = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (page) {
+            _changePage(page);
+            if (page == 0) {
+              Modular.to.navigate('/tabs/home');
+            } else if (page == 1) {
+              Modular.to.navigate('/tabs/discover');
+            } else if (page == 2) {
+              Modular.to.navigate('/tabs/video');
+            } else if (page == 3) {
+              Modular.to.navigate('/tabs/inbox');
+            } else if (page == 4) {
+              Modular.to.navigate('/tabs/profile');
+            }
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.white,
+          currentIndex: _pageIndex,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, size: 30), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search, size: 30), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search, size: 30), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, size: 30), label: 'Message'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person, size: 30), label: 'Profile'),
+          ]),
+      body: RouterOutlet(),
+    );
   }
 }
