c0f132a417d159aa974855ab1fe799ead14a46b2
Eduardo Contiero <eduardocontiero@hotmail.com>
2022-07-04 15:19:58 -0300

    add widgets flutter binding

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
 
